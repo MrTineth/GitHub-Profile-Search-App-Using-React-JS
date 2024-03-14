b18637600a16d14d3ada17cad071904a7245703d
Tineth Pathirage <106744622+MrTineth@users.noreply.github.com>
2024-03-14 22:21:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import GithubSearch from './components/GithubSearch';
+
+
+function App() {
+  return (
+    <div className="App">
+      <GithubSearch/>
+    </div>
+  );
+}
+
+export default App;
